02180a0bdcc934ce825525b899dbb32ec639ced5
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-12-06 00:21:39 +0100

    feat: finally finish day one part two

--- day1/main.c
+++ day1/main.c
@@ -1,36 +1,70 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 #include <stdlib.h>
+
+const char *numbers[] = {
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine" 
+}; 
+
+char match_char(FILE *fptr, char *start_char) {
+  char ch = *start_char;
+  if (!isalpha(ch)) return '\0';
+
+  for (int i = 0; i < sizeof(numbers) / sizeof(char*); i++) {
+    int shift = 0;
+    while (numbers[i][shift] == ch) {
+      if (shift == strlen(numbers[i]) - 1) { 
+        fseek(fptr, -shift, SEEK_CUR);
+        return (i + 1) + '0';      
+      } 
+      ch = fgetc(fptr);
+      shift++;
+    }
+    fseek(fptr, -shift, SEEK_CUR);
+    ch = *start_char;
+  } 
+  return '\0';
+}
 
 int main() {
   FILE *fptr;
   fptr = fopen("input.txt", "r");
-  
+
   int sum = 0;
 
-  char firstChar = '\0';
-  char lastChar = '\0';
+  char num[3];
+  num[0] = '\0';
 
-  char digit[3];
-  char c;
-  
-  for (c = getc(fptr); c != EOF; c = getc(fptr)) {
-    if (c == '\n') {
-      digit[0] = firstChar;
-      digit[1] = lastChar;
-      digit[2] = '\0';
+  char ch;
 
-      sum += atoi(digit);
-
-      firstChar = '\0';
-      lastChar = '\0';
+  while (!feof(fptr)) {
+    ch = fgetc(fptr);   
+    if (ch == '\n') {
+      sum += atoi(num);
+      num[0] = '\0';
+      continue;
     }
-    if (isdigit(c) == 0) continue;
-    if (firstChar == '\0') firstChar = c;
-    lastChar = c;
+
+    char digit = '\0';
+    
+    if (isdigit(ch)) digit = ch; 
+    else digit = match_char(fptr, &ch); 
+
+    if (digit == '\0') continue;
+
+    if (num[0] == '\0') num[0] = digit;
+    num[1] = digit; 
   }
 
   printf("Sum: %d\n", sum);
-
   fclose(fptr);
 }
