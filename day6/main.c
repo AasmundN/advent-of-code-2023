227d3d5adfebcc40cf5ff46ca55d4ed0b370e1e1
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-12-10 23:56:01 +0100

    feat: day six part two

--- day6/main.c
+++ day6/main.c
@@ -1,15 +1,17 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define NUM_RACES 3
 #define MAX_LINE_LENGTH 50
 
 typedef struct {
-  int time;
-  int distance;
+  unsigned long long int time;
+  unsigned long long int distance;
 } Race;
 
+// for part one
 Race **get_races(FILE *fptr) {
   Race **races = malloc(NUM_RACES * sizeof(Race*));
 
@@ -48,11 +50,45 @@ Race **get_races(FILE *fptr) {
   return races;
 }
 
-int find_combinations(Race *race) {
-  int sum = 0;
+// for part two
+Race *get_race(FILE *fptr) {
+  Race *race = malloc(sizeof(Race));
 
-  for (int i = 1; i < race->time; i++) {
-    int distance = i * (race->time - i);
+  char times[MAX_LINE_LENGTH];
+  char distances[MAX_LINE_LENGTH];
+
+  // get lines from file
+  fgets(times, MAX_LINE_LENGTH, fptr);
+  fgets(distances, MAX_LINE_LENGTH, fptr);  
+
+  char *time_ch = &times[0];
+  char *distance_ch = &distances[0];
+
+  char time[MAX_LINE_LENGTH] = {'\0'};
+  char distance[MAX_LINE_LENGTH] = {'\0'};
+
+  while (*time_ch != '\n') {
+    if (isdigit(*time_ch)) strncat(time, time_ch, 1);
+    time_ch++;
+  }
+
+  while (*distance_ch != '\n') {
+    if (isdigit(*distance_ch)) strncat(distance, distance_ch, 1);
+    distance_ch++;
+  }
+
+  race->time = strtol(time, NULL, 10);
+  race->distance = strtol(distance, NULL, 10); 
+
+  rewind(fptr);
+  return race;
+}
+
+unsigned long long int find_combinations(Race *race) {
+  unsigned long long int sum = 0;
+
+  for (unsigned long long int i = 1; i < race->time; i++) {
+    unsigned long long int distance = i * (race->time - i);
     if (distance > race->distance) sum++; 
   }
 
@@ -70,17 +106,19 @@ int main() {
   // free races memory
   int index = 0;
   while (races[index] != NULL) {
-    printf("Race %d: %d - %d\n", index, races[index]->time, races[index]->distance);
-
     product *= find_combinations(races[index]);
 
     free(races[index]);
     index++;
   }
 
-  printf("Product: %d", product);
+  // for part 2
+  Race *race = get_race(fptr);
+
+  printf("Product: %d, Product part two: %llu\n", product, find_combinations(race));
 
   free(races);
+  free(race);
 
   fclose(fptr);
 
